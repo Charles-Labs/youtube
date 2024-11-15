b8822e6e68123a02866abf9b04992337e1f063d0
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-11-15 18:39:20 +0200

    feat: Add method stub for printImportant

--- codingwithjohn/src/main/java/Annotations.java
+++ codingwithjohn/src/main/java/Annotations.java
@@ -1,5 +1,9 @@
 public class Annotations {
     public static void main(String[] arg){
-        System.out.println("Hello world");
+        System.out.println("Hello world of annotations");
+    }
+
+    public static void printImportant(){
+
     }
 }

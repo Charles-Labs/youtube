1d80900db2df67955c628dec6ed4ca6aa0e7f2f9
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-11-23 18:25:18 +0200

    feat: Check if class is important polymorphically

--- codingwithjohn/src/main/java/Annotations.java
+++ codingwithjohn/src/main/java/Annotations.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Annotations {
     public static void main(String[] arg){
         System.out.println("Hello world of annotations");
@@ -8,6 +11,18 @@ public class Annotations {
         Triangle triangle = new Triangle();
         Square square = new Square();
         Circle circle = new Circle();
+
+        List<Shape> shapes = new ArrayList<>();
+        shapes.add(triangle);
+        shapes.add(square);
+        shapes.add(circle);
+
+        for (Shape shape: shapes){
+            if (Shape.class.isAnnotationPresent(Important.class))
+                System.out.println("Shape is important");
+            else
+                System.out.println("Shape is not important");
+        }
 
         if (Triangle.class.isAnnotationPresent(Important.class))
             System.out.println("A triangle is important");

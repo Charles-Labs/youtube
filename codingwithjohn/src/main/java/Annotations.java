48c42eafabb70d811542fc44a6f3245d702980fe
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-11-26 18:23:15 +0200

    feat: Add shape type polymorphically

--- codingwithjohn/src/main/java/Annotations.java
+++ codingwithjohn/src/main/java/Annotations.java
@@ -15,9 +15,9 @@ public class Annotations {
 
         for (Shape shape: shapes){
             if (Shape.class.isAnnotationPresent(Important.class))
-                System.out.println("Shape is important");
+                System.out.println(shape.getType() + " is important");
             else
-                System.out.println("Shape is not important");
+                System.out.println(shape.getType() + " is not important");
         }
 
         if (Triangle.class.isAnnotationPresent(Important.class))

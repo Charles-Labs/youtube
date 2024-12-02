e2e90898967efeda0240d440e37e95f61918e6a9
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-12-02 23:22:13 +0200

    feat: Add Vertex constructor

--- codingwithjohn/src/main/java/Vertex.java
+++ codingwithjohn/src/main/java/Vertex.java
@@ -1,3 +1,9 @@
 public class Vertex {
     private int xPos, yPos, zPos;
+
+    public Vertex(int xPos, int yPos, int zPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.zPos = zPos;
+    }
 }

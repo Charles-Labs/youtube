5680838e38b10e190d66af962f35f0eec2560110
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-12-13 18:52:23 +0200

    feat: Add gravity to the physics engine

--- codingwithjohn/src/main/java/annotations/engine/PhysicsEngine.java
+++ codingwithjohn/src/main/java/annotations/engine/PhysicsEngine.java
@@ -1,4 +1,9 @@
 package annotations.engine;
 
 public class PhysicsEngine extends Engine{
+    private final int gravity;
+
+    public PhysicsEngine(int gravity) {
+        this.gravity = gravity;
+    }
 }

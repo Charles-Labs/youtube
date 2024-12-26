d5d7336ad4a7f27ab581bb6383e5904c527ff5fc
CharlesMoswane <76690068+CharlesMoswane@users.noreply.github.com>
2024-12-26 17:00:00 +0200

    feat: Set EnableGravity annotation to runtime retention

--- codingwithjohn/src/main/java/annotations/jEngine/annotations/EnableGravity.java
+++ codingwithjohn/src/main/java/annotations/jEngine/annotations/EnableGravity.java
@@ -1,5 +1,9 @@
 package annotations.jEngine.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface EnableGravity {
     double g() default 9.8;
 }
